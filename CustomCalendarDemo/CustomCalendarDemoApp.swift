3480d674511940e12fe6be201f172e5645decee4
chanoktrue <chanoktrue@me.com>
2024-03-05 05:52:44 +0700

    first project

---
+++ CustomCalendarDemo/CustomCalendarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarDemoApp.swift
+//  CustomCalendarDemo
+//
+//  Created by Thongchai Subsaidee on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
